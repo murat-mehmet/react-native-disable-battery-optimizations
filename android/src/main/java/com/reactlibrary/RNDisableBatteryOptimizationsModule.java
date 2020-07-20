21a0fe99efe19b0567ad9e0bfacd25f57b7f16c1
Murat <7101489+murat-mehmet@users.noreply.github.com>
2020-07-20 14:48:02 +0300

    Update RNDisableBatteryOptimizationsModule.java

--- android/src/main/java/com/reactlibrary/RNDisableBatteryOptimizationsModule.java
+++ android/src/main/java/com/reactlibrary/RNDisableBatteryOptimizationsModule.java
@@ -1,4 +1,4 @@
-package com.reactlibrary;
+package com.batteryoptimization;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
